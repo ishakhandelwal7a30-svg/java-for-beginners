1070fb5e1b3641f341ea52397370c251d2d9c422
Isha Khandelwal <ishakhandelwal.7a.30@gmail.com>
2026-09-08 18:59:14 +0000

    Add Hello World Java program

---
+++ Main.java
@@ -0,0 +1,12 @@
+public class Main{    /*Every Java program starts with a class definition.
+The class name MUST match the filename.*/
+    public static void main (String[] args){    /* The main method is the entry point of any Java program.
+        'public' means accessible everywhere.
+         'static' means it belongs to the class, not an object.
+         'void' means it returns nothing.
+         'String[] args' can hold command-line arguments.*/
+         System.out.println("Hello World!");  /*This line prints text to the console.
+          println = print line (adds a newline at the end). */
+
+    }
+}
