f4da4b01825d9dc28e4dc9fa4541b3b68075a66e
Isha Khandelwal <ishakhandelwal.7a.30@gmail.com>
2026-09-09 19:10:28 +0000

    Add variable4.java to programs1

---
+++ variable4.java
@@ -0,0 +1,9 @@
+/* write  a program to store the char C in a vaiable named initial*/
+// char is variable type that stores single character in single quotes
+
+public class variable4{
+    public static void main (String[] args){
+        char initial='C';
+        System.out.println("initial='"+initial+"'");
+    }
+}
