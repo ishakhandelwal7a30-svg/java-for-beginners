132c5f85908955959d2049311467926be76e8f49
Isha Khandelwal <ishakhandelwal.7a.30@gmail.com>
2026-09-09 18:50:32 +0000

    Add variable3.java to programs

---
+++ variable3.java
@@ -0,0 +1,10 @@
+/* write a program to declare a variable isloggedIn and assign the value true */
+/* we use boolean here where boolean a variable ttype that stores 2 values true or false 
+we use it by using keyword boolean followed by variable name */
+
+public class variable3{
+    public static void main(String[] args){
+        boolean isloggedIn=true;//while assigning true or false we do not use any quotes and it is always written in lowercase
+        System.out.println("isloogedIn="+isloggedIn);
+    }
+}
