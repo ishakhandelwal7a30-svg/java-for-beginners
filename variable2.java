9e449a3925e400424b752a42862627bc9c028755
Isha Khandelwal <ishakhandelwal.7a.30@gmail.com>
2026-09-09 18:41:23 +0000

    Add variable2.java to programs

---
+++ variable2.java
@@ -0,0 +1,9 @@
+/* write a program to store the string I am learning Java in a variable learn */
+// where string type is a special type that consists of multiple chars i.e characters
+
+public class variable2{
+    public static void main(String[] args){
+        String learn ="I am learning Java"; // where learn is the variable name  and the string value is enclosed in double quotes
+        System.out.println("learn=\""+learn+"\"");
+    }
+}
