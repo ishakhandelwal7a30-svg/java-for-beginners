86eb1e06781f143f126a69dc92a27f6d8f132cf3
Isha Khandelwal <ishakhandelwal.7a.30@gmail.com>
2026-09-09 18:31:34 +0000

    Add variable1.java program

---
+++ variable1.java
@@ -0,0 +1,13 @@
+/* write a java program that initialises and declares the following variables
+1. variable name is quantity and initialise the value is 5
+2. double variable named itemPrice value is 24.99
+use system.out.println to display the variables with values */
+
+public class variable1{
+    public static void main(String[] args){
+        int quantity=5; //where int is variable type and quantity is variable name 
+        double itemPrice=24.99;// here double is the varible type showing the value is decimal value
+        System.out.println("Quantity: "+quantity);
+        System.out.println("Item Price: "+itemPrice);
+    }
+}
