963f96bdf709c739df95ef1ba3cc0ffbf7d7df38
Isha Khandelwal <ishakhandelwal.7a.30@gmail.com>
2026-09-12 18:35:42 +0000

    Add type_casting1.java to programs

---
+++ type_casting1.java
@@ -0,0 +1,12 @@
+/* declare a double variable name price and initialise its vale 99.99 
+1. cost price variable to an int and stores result in intPrice
+2. print value of Price and intPrice */
+
+public class type_casting1 {
+    public static void main(String[] args) {
+        double price = 99.99;
+        int intPrice =(int)price;
+        System.out.println("Price: " + price);
+        System.out.println("Int Price: " + intPrice);
+    }
+}
