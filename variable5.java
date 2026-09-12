0a8f23384d354902ea08fc66ba51a841b530b062
Isha Khandelwal <ishakhandelwal.7a.30@gmail.com>
2026-09-12 18:11:29 +0000

    Add variable5.java to program

---
+++ variable5.java
@@ -0,0 +1,7 @@
+/* write a code that initialises thr following variable PI and assigns the value 3.1419 */
+public class variable5{
+    public static void main(String[] args){
+        final double PI=3.1419; // here final is the keyword to declare 
+        System.out.println("PI:"+PI);
+    }
+}
