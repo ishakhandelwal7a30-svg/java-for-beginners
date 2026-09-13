66a15f3fcc3a4f5d8fbaee8f16e3a861bb0bd017
Isha Khandelwal <ishakhandelwal.7a.30@gmail.com>
2026-09-13 17:25:28 +0000

    Add operators1.java to programs

---
+++ operators1.java
@@ -0,0 +1,10 @@
+/* Write a code that initializes two variables, a and b with values 5.2 and 2.6 
+after that initialize another variable c that will hold the result of a/b */
+ public class operators1{
+    public static void main(String[] args){
+        double a=5.2;
+        double b=2.6;
+        double c=a/b;
+        System.out.println("a="+a+ ",b="+b+ ",c="+c);
+    }
+ }
