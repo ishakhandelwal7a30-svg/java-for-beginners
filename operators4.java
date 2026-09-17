d5096f6932479ca9c624eb48fef81fbe4423162d
Isha Khandelwal <ishakhandelwal.7a.30@gmail.com>
2026-09-17 13:50:56 +0000

    Add operators4.java to programs

---
+++ operators4.java
@@ -0,0 +1,12 @@
+/*Write a script that initializes 2 variables n1 and n2 with the values 8 and 9 (accordingly).
+After that initialize another variable n3 that will hold whether n1 is bigger than n2.*/
+
+public class operators4 {
+    public static void main(String[] args){
+        int n1=8;
+        int n2=9;
+        boolean n3=n1>n2;
+        System.out.println("n1:"+n1+",n2:"+n2+",n3:"+n3);
+    }
+    
+}
